116312b86f0b0ac6d0233314b11c13117160b9c4
Frank Taylor <frank@franktaylor.io>
2018-03-22 02:38:24 -0700

    initial node commit

---
+++ node/README.md
@@ -0,0 +1,37 @@
+# sslsaas-node
+Sample code that demonstrates how to issue SSL certificates for custom hostnames using Promises in Node.js.
+
+## Installation
+
+Requires Node version v5.0.0 or later
+```
+npm install
+```
+## Usage
+
+To get started, open up `app.js` and modify the parameters for `issueCert()`:
+```javascript
+issueCert({
+  customerHostname: 'store.nikitamarie.com',
+  validationMethod: 'cname',
+  customOriginServer: 'dedicated-sneaker-server.saasprovider.com' // Optional
+})
+```
+**`customerHostname`**
+The customer subdomain to which you want to issue a certificate
+
+**`validation`**
+The method you're using to verify customer's domain ownership (`'cname'` or `'http'`)
+
+**`customOriginServer`**  _[Optional]_
+The address for the custom origin server designated for this customer. If you choose to specify this param, there MUST be a valid DNS or LB record (or wildcard) in your Cloudflare account before a certificate will be issued.
+
+### Set the environment variables and run the app:
+
+To authenticate and run the code, just specify three environment variables and run `node app.js`:
+```
+CF_API_KEY="YOURAPIKEY" CF_API_EMAIL="YOUREMAIL" CF_ZONE="YOURMAINZONE" node app.js
+```
+> `CF_ZONE` is the DNS zone that's administering the service (e.g. saasprovider.com)
+
+Once you start the server, a cron job makes calls every 60 seconds to Cloudflare's API until your customer's certificate status changes to 'active'.
---
+++ node/issuecert.js
@@ -0,0 +1,127 @@
+const cron = require('node-cron');
+const fetch = require('node-fetch');
+
+const config = {
+  "apiKey": process.env.CF_API_KEY,
+  "email": process.env.CF_API_EMAIL,
+  "parentZoneName": process.env.CF_ZONE
+}
+
+const IssueCert = ({
+  customerHostname,
+  validationMethod,
+  customOriginServer
+}) => {
+
+  // Set required headers for all Cloudflare API calls
+  const hdrs = {
+    "Content-Type": "application/json",
+    "X-Auth-Email": config.email,
+    "X-Auth-Key": config.apiKey
+  };
+
+  //
+  let zoneID = '';
+
+  const customHostnamePayload = () => {
+
+    // Set the customcustomHostnamePayload and validation method for provisioning your customer's cert
+    let jsonPayload = {
+      "hostname": customerHostname,
+      "ssl": {
+        "method": validationMethod,
+        "type": "dv"
+      }
+    };
+
+    // If specified in customOriginServer parameter, amend the jsoncustomcustomHostnamePayload to
+    // include a custom origin server
+    if (customOriginServer !== undefined) {
+      Object.defineProperty(jsonPayload, "custom_origin_server", {
+        value: customOriginServer
+      })
+    }
+    return JSON.stringify(jsonPayload);
+  };
+
+
+
+  // Return a promise that returns the zoneID for your whitelabeled zone when fulfilled
+  const validateZone = () => {
+    return Promise.resolve(fetch('https://api.cloudflare.com/client/v4/zones', {
+        method: 'GET',
+        headers: hdrs
+      })
+      .then(res => res.json())
+      .then(json => {
+        let i = 0;
+        while (i < json.result.length) {
+          if (json.result[i].name === config.parentZoneName) {
+            // Log and return the zoneID that corresponds with your whitelabeled zone name
+            console.log(json.result[i].id);
+            zoneID = json.result[i].id
+            return zoneID;
+            break;
+          }
+          i++;
+        }
+      }))
+  };
+
+  const requestCert = () => {
+    return fetch(`https://api.cloudflare.com/client/v4/zones/${zoneID}/custom_hostnames`, {
+        method: 'POST',
+        body: customHostnamePayload(),
+        headers: hdrs
+      })
+      .then(res => res.json())
+      .then(body => {
+        console.log(body);
+        return (body.success).toString();
+      })
+  };
+
+  const getCertStatus = () => {
+    return fetch(`https://api.cloudflare.com/client/v4/zones/${zoneID}/custom_hostnames?hostname=${customerHostname}`, {
+        method: 'GET',
+        headers: hdrs
+      })
+      .then(res => res.json())
+      .then(body => {
+        return printCertDetailsFromEdge(body)
+      })
+  };
+
+  const printCertDetailsFromEdge = (body) => {
+    console.log(body);
+    if (typeof body.result.ssl === 'string') {
+      switch (body.result.ssl.status) {
+        case 'pending_validation':
+          console.log(`${customerHostname} still awaiting ${validationMethod} validation`)
+          break;
+
+        case 'active':
+          console.log(`Certificate Provisioned for: ${customerHostname}`)
+          runCron.destroy();
+          break;
+
+        default:
+          console.log(`${customerHostname} status unknown`)
+          break;
+      }
+    }
+  };
+
+  const runCron = cron.schedule('* * * * *', () => {
+    return getCertStatus();
+  })
+
+  return validateZone()
+    .then(() => {
+      return requestCert()
+    }).then(() => {
+      runCron;
+    })
+}
+
+module.exports = IssueCert;
